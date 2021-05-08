3cee185445739ed4cc4d4d4d9747de1d3a9d4ece
bohdanloik <bohdanloik@gmail.com>
2021-05-08 17:43:40 +0300

    add css style

--- src/Components/Todolist/Todolist.tsx
+++ src/Components/Todolist/Todolist.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import List from './List/List';
+import style from './style.module.css'
 function Todolist() {
     return(
-      <div className="todoList">
+      <div className={style.todoList}>
         <Header />
         <List />
         <Footer />
