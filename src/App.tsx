7763f11f4d5d12d4172b2bf799888cf72fca2bb2
bohdanloik <bohdanloik@gmail.com>
2021-05-08 15:02:18 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import List from './List';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="todoList">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+  );
+}
+
+
+function Footer() {
+  return (
+  <div className="todoList-footer">
+    <button>All</button>
+    <button>Completed</button>
+    <button>Active</button>
+  </div>
   );
 }
 
