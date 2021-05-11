160e8ac5eb3d00e448d5a9aa0c355ecbc2905ccd
bohdanloik <bohdanloik@gmail.com>
2021-05-11 15:14:45 +0300

    add style for task component

--- src/Components/Todolist/List/Task/Task.tsx
+++ src/Components/Todolist/List/Task/Task.tsx
@@ -6,8 +6,9 @@ type PropsType = {
   title: string;
 };
 export const Task = (props: PropsType) => {
+  const css = props.isDone ? styles.done : '';
   return (
-    <div className={styles.task}>
+    <div className={styles.task + " " + css}>
       <input type="checkbox" checked={props.isDone} />
       <span>{props.title}</span>
     </div>
