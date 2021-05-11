1b3eb79291f55501617a8f034f045ca42a7a353f
bohdanloik <bohdanloik@gmail.com>
2021-05-11 12:40:48 +0300

    add List PropsType

--- src/Components/Todolist/List/List.tsx
+++ src/Components/Todolist/List/List.tsx
@@ -3,23 +3,22 @@ import styles from './style.module.css'
 function List() {
     return (
       <div className={styles.list}>
-        <div className={styles.task}>
-           <input type="checkbox" checked={true}/>
-           <span>CSS</span>
-         </div>
-      <div className={styles.task}>
-          <input type="checkbox" checked={false}/>
-          <span>JS</span>
-      </div>
-      <div className={styles.task}>
-          <input type="checkbox" checked={false}/>
-          <span>ReactJS</span>
-      </div>
-      <div className={styles.task}>
-          <input type="checkbox" checked={true}/>
-          <span>Patterns</span>
-      </div>
+        <Task isDone={false}  title ='Css'/>
+        <Task isDone={true}  title ='Html'/>
+        <Task isDone={true}  title ='Js'/>
     </div>
     );
+}
+type PropsType = {
+  isDone: boolean;
+  title: string;
+}
+const Task = (props: PropsType) => {
+  return (
+    <div className={styles.task}>
+      <input type="checkbox" checked={props.isDone}/>
+      <span>{props.title}</span>
+    </div>
+  )
 }
 export default List;
