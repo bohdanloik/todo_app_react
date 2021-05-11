718037bcc91855738a3d91d872b316f514b40738
bohdanloik <bohdanloik@gmail.com>
2021-05-11 13:07:09 +0300

    add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import Todolist from './Components/Todolist/Todolist'
 import styles from './App.module.css';
 
 function App() {
+
+  const tasks = [
+    {title:'css',isDone: true},
+    {title:'js',isDone: false},
+    {title:'html',isDone: true}
+  ]
   return (
       <div className = {styles.App}>
         <Todolist />
